7b1778e09549a8f07b4fc1e75f1eabfcece6edb2
keshavcodex <keshavcodex@gmail.com>
2024-03-23 23:51:28 +0530

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import Connection from "./db.js";
-import Routes from './routes/route.js'
+import Connection from './db.js';
+import cors from 'cors';
+import Routes from './routes/route.js';
 
 dotenv.config();
 
@@ -9,7 +10,8 @@ const PORT = process.env.PORT;
 Connection(process.env.MONGO_URL);
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.use("/", Routes);
+app.use('/', Routes);
